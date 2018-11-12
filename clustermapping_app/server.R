0528d8d571d95a76006dc5c798decf987fe8228b
Aous Abdo <aousabdo@gmail.com>
2018-11-12 18:20:31 -0500

    fixed a bug in region matching

--- clustermapping_app/server.R
+++ clustermapping_app/server.R
@@ -1,44 +1,57 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+
 source("./global.R")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
   output$text <- renderDataTable({
-    # get regions name from input
+    
+    # get region's name from input
     region_name <- input$region_name
     
-    selected_region <- regions_dt[region_short_name_t %like% region_name
+    # filter the regions data.table for the selected region
+    selected_region <- regions_dt[region_short_name_t == region_name
                                   , .(region_type_t, region_code_t, name_t, region_short_name_t)]
+
   
-    selected_region <- jsonlite::fromJSON(paste(base_url, "region", selected_region[, region_type_t], selected_region[, region_code_t], input$year, sep = "/"))
+    # in some cases, the region_code is between 1 and 9, in this case
+    # if we call the api with this integer we'll get an error since the 
+    # api expects a 01, 02, etc. so we need to fix this
+    region_code <- selected_region[, region_code_t]
+    if(nchar(region_code) == 1) region_code = paste0("0", region_code)
+    
+    # get general data about the selected region
+    query <- paste(base_url, "region", selected_region[, region_type_t]
+                   , region_code, input$year, sep = "/")
+    
+    selected_region <- jsonlite::fromJSON(query)
+    
+    # convert to data.table object
     selected_region <- as.data.table(selected_region)
     
+    # get a list of strong clusters for selected region
     strong_clusters <- selected_region$strong_clusters
 
+    # prepare strong-clusters data
     cluster_name <- sapply(strong_clusters, function(x) x$name)
     cluster_code <- sapply(strong_clusters, function(x) x$code)
     cluster_key  <- sapply(strong_clusters, function(x) x$key)
     cluster_pos  <- sapply(strong_clusters, function(x) x$pos)
     
+    # bind strong-cluster data in one data.table object
     strong_clusters <- cbind(cluster_name, cluster_code, cluster_key, cluster_pos)
     
     strong_clusters <- as.data.table(strong_clusters)
+    
     strong_clusters[, cluster_code := as.integer(cluster_code)]
     strong_clusters[, cluster_pos  := as.integer(cluster_pos)]
     strong_clusters[, cluster_name := factor(cluster_name)]
     strong_clusters[, cluster_key  := factor(cluster_key)]
     
     strong_clusters <- unique(strong_clusters)
+    
+    # set data.table key
     setkey(strong_clusters, cluster_key)
     return(strong_clusters)
   })
