e9b84bca1630c70db9992f0bc856a7ade0257856
Aous Abdo <aousabdo@gmail.com>
2018-11-28 11:02:29 -0500

    added pars to code

--- clustermapping_app/bubble_charts.R
+++ clustermapping_app/bubble_charts.R
@@ -1,27 +1,34 @@
 library(ggplot2)
 
 # function to calculate coords of a circle
-circle <- function(center, radius) {
-  th <- seq(0, 2*pi, len=1000)
-  data.frame(x = center[1] + radius*cos(th), y = center[2] + radius*sin(th))
+circle <- function(center, radius, res = 1000) {
+  # we will calculate the coords of a circle using the parametric equations of a circle
+  # calcualte theta, the parametric angle
+  th <- seq(0, 2*pi, len = res)
+  
+  # coordinates of the circle
+  DT <- data.table(x = center[1] + radius*cos(th), y = center[2] + radius*sin(th))
+  return(DT)
 }
 
-N <- 10
+N <- 3
+
 # example dataset
-df  <- data.table(cluster = paste("Cluster", LETTERS[1:N])
-                  , start = 1000*(N:1)
+df  <- data.table(cat_var = paste("Cluster", LETTERS[1:N])
+                  , value = 1000*(N:1)
                   , end = 500*(N:1)
-                  , pos = sample(1:N, N))                       
+                  , pos = sample(1:N, N)
+                  )                       
 
 setkey(df, pos)
 setorder(df, pos)
 
-max       <- max(df$start)
+max       <- max(df$value)
 n.bubbles <- nrow(df)
-scale     <- 0.4/sum(sqrt(df$start))
+scale     <- 0.4/sum(sqrt(df$value))
 
 # calculate scaled centers and radii of bubbles           
-radii <- scale*sqrt(df$start)
+radii <- scale*sqrt(df$value)
 ctr.x <- cumsum(c(radii[1], head(radii, -1) + tail(radii, -1) + .01))
 
 center_aligned <- TRUE
@@ -29,10 +36,10 @@ center_aligned <- TRUE
 # starting (larger) bubbles
 if(center_aligned){
   gg.1   <- do.call(rbind, lapply(1:n.bubbles, function(i) cbind(group = i, circle(c(ctr.x[i], radii[1]), radii[i]))))
-  text.1 <- data.frame(x = ctr.x, y = radii[1], label = paste(df$cluster, df$start, sep = "\n"))
+  text.1 <- data.frame(x = ctr.x, y = radii[1], label = paste(df$cat_var, df$value, sep = "\n"))
 }else{
   gg.1   <- do.call(rbind, lapply(1:n.bubbles, function(i) cbind(group = i, circle(c(ctr.x[i], radii[i]), radii[i]))))
-  text.1 <- data.frame(x = ctr.x, y = radii, label = paste(df$cluster, df$start, sep = "\n"))
+  text.1 <- data.frame(x = ctr.x, y = radii, label = paste(df$cat_var, df$value, sep = "\n"))
 }
 
 add_inner_circles <- FALSE
@@ -41,7 +48,7 @@ add_inner_circles <- FALSE
 p <- ggplot() + geom_polygon(data = gg.1, aes(x, y, group = group), fill = "seagreen") +
   geom_path(data = gg.1, aes(x, y, group = group), color = "grey50") +
   geom_text(data = text.1, aes(x, y, label = label), col = "white") +
-  labs(x = "", y = "") + scale_y_continuous(limits = c(-0.1, 2.5*scale*sqrt(max(df$start)))) +
+  labs(x = "", y = "") + scale_y_continuous(limits = c(-0.1, 2.5*scale*sqrt(max(df$value)))) +
   coord_fixed() +
   theme(axis.text = element_blank()
         , axis.ticks = element_blank()
@@ -66,5 +73,4 @@ if(add_inner_circles){
 
 print(p)
 
-gg <- ggplotly(p)
-print(gg)
+
