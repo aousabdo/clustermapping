4c6f78c7400e4d038b35d916e3dd3025167d68c9
Aous Abdo <aousabdo@gmail.com>
2018-12-06 21:19:37 -0800

    added code to handle nodes with no edges

--- clustermapping_app/plotly_network_viz_2.R
+++ clustermapping_app/plotly_network_viz_2.R
@@ -17,8 +17,17 @@ cluster_data_out$related_clusters_dt[, .(parent_cluster_name, cluster_name_t, re
 
 
 all_related_clusters <- get_all_related_clusters(clusters_list_input = clusters_list)
-all_related_clusters[, .(parent_cluster_name, cluster_name_t, related_avg)] %>% graph.data.frame() -> net
+all_related_clusters[complete.cases(all_related_clusters), .(parent_cluster_name, cluster_name_t, related_avg)] %>% graph.data.frame() -> net
 
+
+net <- add_vertices(net, 1, name = "Jesse")
+
+clusters_with_no_related_clusters <- all_related_clusters[!complete.cases(all_related_clusters), parent_cluster_name]
+
+for(i in clusters_with_no_related_clusters){
+  net <- add_vertices(net, 1, name = i)
+}
+set.seed(pi)
 V(net)$degree <- centralization.degree(net)$res
 df_net <- ggnetwork(net, layout = "fruchtermanreingold", weights="related_avg", niter=5000)
 # df_net <- ggnetwork(net, layout = "fruchtermanreingold", weights="weight", niter=5000)
